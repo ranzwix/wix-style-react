0015aee009fa2856c2edb83e5d7a457830089bcb
Lior Belinsky <liorbe@wix.com>
2017-01-15 21:18:58 +0200

    added template testkit for Input

--- testkit/index.js
+++ testkit/index.js
@@ -3,8 +3,10 @@ import {testkitFactoryCreator} from '../src/test-common';
 import buttonSelectionDriverFactory from '../src/ButtonSelection/ButtonSelection.driver';
 export const buttonSelectionTestkitFactory = testkitFactoryCreator(buttonSelectionDriverFactory);
 
+import inputDriverFactory from '../src/Input/Input.driver';
+export const inputTestkitFactory = testkitFactoryCreator(inputDriverFactory);
+
 export {buttonTestkitFactory} from '../src/Button/testkit/Button';
-export {inputDriverFactory} from '../src/Input/Input.driver';
 export {checkboxDriverFactory} from '../src/Checkbox/Checkbox.driver';
 export {radioGroupDriverFactory} from '../src/RadioGroup/RadioGroup.driver';
 export {toastTestkitFactory} from '../src/Toast/testkit/Toast';
